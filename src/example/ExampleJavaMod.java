485e25cbdd6eddb445eced07a69edf1c75b63662
AntiBlast <arrassenzu.gmail.com>
2025-11-03 17:25:27 +0300

    test 3

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -18,15 +18,6 @@ public class ExampleJavaMod extends Mod{
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
         });
     }
 
--- src/example/content/units/NeoUnits.java
+++ src/example/content/units/NeoUnits.java
@@ -38,42 +38,87 @@ import static mindustry.Vars.*;
 
 public class NeoUnits {
 
-    public static ErekirUnitType spidern;
+    public static NeoplasmUnitType spidern;
 
     public static void load(){
 
-        spidern = new ErekirUnitType("spidern"){{
-            constructor = EntityMapping.map(3);
-            speed = 0.72f;
+        spidern = new NeoplasmUnitType("spidern"){{
+            constructor = LegsUnit::create;
+            speed = 0.9f;
             drag = 0.11f;
-            hitSize = 9f;
+            hitSize = 11f;
             rotateSpeed = 3f;
-            health = 680;
-            armor = 4f;
+            health = 150;
+            armor = 1f;
             legStraightness = 0.3f;
             stepShake = 0f;
+            drawCell = false;
 
-            legCount = 6;
-            legLength = 8f;
+            legCount = 4;
+            legLength = 10f;
             lockLegBase = true;
             legContinuousMove = true;
-            legExtension = -2f;
-            legBaseOffset = 3f;
-            legMaxLength = 1.1f;
-            legMinLength = 0.2f;
-            legLengthScl = 0.96f;
-            legForwardScl = 1.1f;
-            legGroupSize = 3;
+            legExtension = -7.5f;
+            legBaseOffset = 2f;
+            legMaxLength = 2.1f;
+            legMinLength = 1.4f;
+            legLengthScl = 1.1f;
+            legForwardScl = 1.3f;
+            legSpeed = 0.2f;
             rippleScale = 0.2f;
 
-            legMoveSpace = 1f;
+            legMoveSpace = 1.2f;
             allowLegStep = true;
-            hovering = true;
             legPhysicsLayer = false;
 
-            shadowElevation = 0.1f;
-            groundLayer = Layer.legUnit - 1f;
-            targetAir = false;
+                weapons.add(new Weapon("spidern-weapon"){{
+                shootSound = Sounds.missile;
+                mirror = false;
+                showStatSprite = false;
+                x = 0f;
+                y = 5f;
+                shootY = 4f;
+                reload = 63f;
+                cooldownTime = 42f;
+                heatColor = Pal.turretHeat;
+
+                bullet = new ArtilleryBulletType(3f, 40){{
+                    shootEffect = new MultiEffect(Fx.shootSmallColor, new Effect(9, e -> {
+                        color(Color.white, e.color, e.fin());
+                        stroke(0.7f + e.fout());
+                        Lines.square(e.x, e.y, e.fin() * 5f, e.rotation + 45f);
+
+                        Drawf.light(e.x, e.y, 23f, e.color, e.fout() * 0.7f);
+                    }));
+
+                    collidesTiles = true;
+                    backColor = hitColor = Pal.techBlue;
+                    frontColor = Color.white;
+
+                    knockback = 0.8f;
+                    lifetime = 46f;
+                    width = height = 9f;
+                    splashDamageRadius = 19f;
+                    splashDamage = 30f;
+
+                    trailLength = 27;
+                    trailWidth = 2.5f;
+                    trailEffect = Fx.none;
+                    trailColor = backColor;
+
+                    trailInterp = Interp.slope;
+
+                    shrinkX = 0.6f;
+                    shrinkY = 0.2f;
+
+                    hitEffect = despawnEffect = new MultiEffect(Fx.hitSquaresColor, new WaveEffect(){{
+                        colorFrom = colorTo = Pal.techBlue;
+                        sizeTo = splashDamageRadius + 2f;
+                        lifetime = 9f;
+                        strokeFrom = 2f;
+                    }});
+                }};
+            }});
         }};
 }
 }
